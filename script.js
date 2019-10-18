d91eed7374d6fb325352e4d1700474ab0476b0b5
Mobolanle Adebesin <bolaadebesin@gmail.com>
2019-10-18 15:25:24 -0400

    The 'type' button functions correctly

--- script.js
+++ script.js
@@ -14,16 +14,21 @@ fetch(url+ "pokemon/6/")
                 //     console.log(res.moves[i].move.name)
                 // }
                 let buttons = document.querySelectorAll(".button");
+                let body = document.querySelector("body");
                 for(let i = 0; i < buttons.length; i++){
                 buttons[i].addEventListener("click", divAppear); 
 
                 function divAppear(evt){
                     let buttonText = evt.target.innerText;
+                    body.style.backgroundColor = "black"
+                    body.style.opacity = 0.9;
+
                   if(buttonText === "Type"){
                      let  divType = document.getElementById("modal-type");
-                     divType.innerText = `Charizard is both ${res.types[0].type.name} type and ${res.types[1].type.name} type! `
+                     let divh1 = document.querySelector(".divh1");
+                     divh1.innerText = `Charizard is both ${res.types[0].type.name} type and ${res.types[1].type.name} type! `
                     divType.classList.toggle("hidden");
-                     //   console.log(`Charizard is both ${res.types[0].type.name} type and ${res.types[1].type.name} type! `);
+                    
                   }
                 }
 
