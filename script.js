5a532662b6e51ae016585cd1b80069382a0c8087
Mobolanle Adebesin <bolaadebesin@gmail.com>
2019-10-18 16:21:04 -0400

    Completed buttons Moves, Weight, Height, Abilities

--- script.js
+++ script.js
@@ -25,10 +25,39 @@ fetch(url+ "pokemon/6/")
 
                   if(buttonText === "Type"){
                      let  divType = document.getElementById("modal-type");
-                     let divh1 = document.querySelector(".divh1");
-                     divh1.innerText = `Charizard is both ${res.types[0].type.name} type and ${res.types[1].type.name} type! `
+                     let divTypeh1 = document.querySelector(".divTypeh1");
+                     divTypeh1.innerText = `Charizard is both ${res.types[0].type.name} type and ${res.types[1].type.name} type! `
                     divType.classList.toggle("hidden");
                     
+                  }
+                  else if(buttonText === "Moves"){
+                      let moves = [];
+                    for (let i = 0; i < 5; i++){
+                         moves.push(res.moves[i].move.name);
+                        }
+                      let divMoves = document.getElementById("modal-moves");
+                      let divMovesh1 = document.querySelector(".divMovesh1");
+                    //   divMovesh1.innerText = 
+                    let charizardMoves = "Charizards moves are: \n"
+                    for( let j = 0; j < moves.length; j++){
+                        charizardMoves = charizardMoves + "\n" + moves[j];
+                    }
+                    divMovesh1.innerText = charizardMoves;
+                    divMoves.classList.toggle("hidden");
+                      
+                  }
+                  else if(buttonText === "Weight"){
+                      let divWeight = document.getElementById("modal-weight");
+                      let divWeighth1 = document.querySelector(".divWeighth1");
+                      divWeighth1.innerText = `Charizard's weight is ${res.weight}`;
+                      divWeight.classList.toggle("hidden");
+                  }
+                  else if(buttonText === "Abilities"){
+                      let divAbilities = document.getElementById("modal-abilities");
+                      let divAbilitiesh1 = document.querySelector(".divAbilitiesh1");
+                     
+                      divAbilitiesh1.innerText = `Charizard's abilities are ${res.abilities[0].ability.name} and ${res.abilities[1].ability.name}!`;
+                      divAbilities.classList.toggle("hidden");
                   }
                 }
 
